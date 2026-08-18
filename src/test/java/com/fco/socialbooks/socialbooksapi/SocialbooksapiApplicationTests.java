5fbd1c266ac2d567c771161cd844834e468087fc
felipecardosodeoliveira <felipecardosodeoliveira02@gmail.com>
2026-08-17 23:02:04 -0300

    Initial commit

---
+++ src/test/java/com/fco/socialbooks/socialbooksapi/SocialbooksapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fco.socialbooks.socialbooksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialbooksapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
